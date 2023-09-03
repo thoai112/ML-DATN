e3a240f3857361cb0dcc88cc1ecf1ec0c43f7c9e
Kuratajr <thoailata1000@gmail.com>
2023-09-03 19:29:55 +0700

    up

--- airflow/dags/ecsoperator_2.py
+++ airflow/dags/ecsoperator_2.py
@@ -9,10 +9,10 @@ from airflow.utils.dates import days_ago
 import boto3
 import datetime as dt
 
-CLUSTER_NAME="ml-cluster" #Replace value with your information
-CONTAINER_NAME="ecs-op-task" #Replace value with your information
+CLUSTER_NAME="ml-cluster" 
+CONTAINER_NAME="ecs-op-task-1" 
 LAUNCH_TYPE="FARGATE"
-SERVICE_NAME="ecs-ttst-service" #Replace value with your information
+SERVICE_NAME="ecs-ttst-service-1" 
 
 with DAG(
     dag_id = "ecs_fargate_dag_1",
